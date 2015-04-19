d64cc87bdff56df13a019a04fa6e0c8cecd1bf7a
ArktinenSieni <matti.raty@helsinki.fi>
2015-04-19 18:54:12 +0300

    pari testiä

--- viiteapuri/src/test/java/hammentyneethaxaajat/viiteapuri/validaattori/ValidaattoriTest.java
+++ viiteapuri/src/test/java/hammentyneethaxaajat/viiteapuri/validaattori/ValidaattoriTest.java
@@ -11,19 +11,18 @@ import java.util.ArrayList;
 import java.util.List;
 import static org.junit.Assert.fail;
 
-
 public class ValidaattoriTest {
+
     Validaattori validaattori;
     ViiteKasittelija kasittelija;
-    
-    
+
     @Before
     public void setUp() {
         kasittelija = mock(ViiteKasittelija.class);
         validaattori = new Validaattori(kasittelija);
-        
+
     }
-    
+
     @Test
     public void validoiTyypin() {
         try {
@@ -32,7 +31,7 @@ public class ValidaattoriTest {
             fail("Tyypin validointi epäonnistui");
         }
     }
-    
+
     @Test
     public void validoiNimen() {
         try {
@@ -41,33 +40,32 @@ public class ValidaattoriTest {
             fail("Nimen validointi epäonnistui");
         }
     }
-    
+
     @Test
     public void validoiRistiviite() {
         Viite viite = when(mock(Viite.class).getNimi()).thenReturn("aapinen").getMock();
         List<Viite> viitteet = new ArrayList<Viite>();
         viitteet.add(viite);
-        
+
         when(kasittelija.getViitteet()).thenReturn(viitteet);
-        
+
         try {
             validaattori.validoi("crossref", "aapinen");
         } catch (Exception e) {
             fail("Ristiviitteen validointi epäonnistui");
         }
     }
-   
 
     @Test
     public void viiteTyypinValidointiOnnistuuTunnetuillaViiteTyypeilla() {
         validaattori.validoiViiteTyyppi("book");
     }
-    
-    @Test (expected = IllegalArgumentException.class)
+
+    @Test(expected = IllegalArgumentException.class)
     public void viiteTyypinValidointiHeittaaExceptioninJosTuntematonViiteTyyppi() {
         validaattori.validoiViiteTyyppi("kirja");
     }
-    
+
     @Test
     public void viiteTyypinValidoinninHeittamaExceptionSisaltaaJarkevanIlmoituksen() {
         try {
@@ -76,55 +74,65 @@ public class ValidaattoriTest {
             assertTrue(e.getMessage().contains(Tulosteet.TUNTEMATON_VIITETYYPPI));
         }
     }
-    
+
     @Test
     public void nimenValidointiOnnistuuJosSellaistaEiOleMuillaViitteillaJaSyntaksiOK() {
         validaattori.validoiNimi("asdas");
     }
-    
-    @Test (expected = IllegalArgumentException.class)
+
+    @Test(expected = IllegalArgumentException.class)
     public void nimenValidointiEiOnnistuJosToisellaViitteellaOnSamaNimi() {
         Viite viite = when(mock(Viite.class).getNimi()).thenReturn("aapinen").getMock();
         List<Viite> viitteet = new ArrayList<Viite>();
         viitteet.add(viite);
-        
+
         when(kasittelija.getViitteet()).thenReturn(viitteet);
         validaattori.validoiNimi("aapinen");
     }
-    
-    
+
     @Test
     public void attribuutinvalidointiOnnistuuJosAttribuuttiOnMaarattyaMuotoa() {
         validaattori.validoiAttribuutinArvo("key", "123");
     }
-    
-    @Test (expected = IllegalArgumentException.class)
+
+    @Test(expected = IllegalArgumentException.class)
     public void attribuutinvalidointiEiOnnistuJosAttribuuttiEiOleMaarattyaMuotoa() {
         validaattori.validoiAttribuutinArvo("key", "123a");
     }
-    
-    @Test (expected = IllegalArgumentException.class)
+
+    @Test(expected = IllegalArgumentException.class)
     public void attribuutinvalidointiEiOnnistuJosValidoitavanNimellaOlevaaAttribuuttiaEiOlemassa() {
         validaattori.validoiAttribuutinArvo("attr", "abc");
     }
-    
-    @Test (expected = IllegalArgumentException.class)
+
+    @Test(expected = IllegalArgumentException.class)
     public void nimenOnVastattavaSyntaksia() {
         validaattori.validoi("nimi", "@%///&&##");
     }
-    
+
     @Test
     public void ristiViitteenValidointiOnnistuuJosHaettavaTyhjaString() {
         validaattori.validoiViite("");
     }
-    
-    @Test (expected = IllegalArgumentException.class)
+
+    @Test(expected = IllegalArgumentException.class)
     public void ristiViitteenValidointiEiOnnistuJosHaettavaaViiteEiOlemassaJaHaettavaEpaTyhja() {
         validaattori.validoiViite("kirja");
     }
-    
-    @Test (expected = IllegalArgumentException.class)
+
+    @Test(expected = IllegalArgumentException.class)
     public void heitaExceptionHeittaaIllegalArgumentExceptionin() {
         validaattori.heitaException(null);
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void viiteEiSaaOllaTyhja() {
+        validaattori.validoi("viite", "");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void tuntematonValidoitavaHeittaaExceptionin() {
+        validaattori.validoi("kukka", "ohai");
+    }
+
 }
